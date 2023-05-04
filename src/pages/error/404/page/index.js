0ba9cb2eef5deaace36ae3c0cc6016db3f1af747
BrunitoNFT <brunomembrado10@gmail.com>
2023-05-04 09:16:14 -0300

    Error 404 page, error 500 page and server deleted

--- src/pages/error/404/page/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './index.module.css';
-
-const Error404Page = () => {
-  return <div className={style.test}>Error 404 page</div>;
-};
-
-export default Error404Page;
