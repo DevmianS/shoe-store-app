c967e99db9c22dffdd757dfc66b6adb937ae621e
DevmianS <damiansobierajdev@gmail.com>
2023-05-04 20:04:27 +0200

    feat: customized TextField MUI component to fit the design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,27 @@
 import React from 'react';
 import Button from '@/components/UI/Button/Button';
+import {TextField} from '@mui/material';
 
 const Index = () => {
   return (
-    <div>
-      <h1>Test icons usage</h1>
-      <i className="icon-chevron-down"></i>
-      <i className="icon-gallery"></i>
-      <i className="icon-logo"></i>
-      <i className="icon-logout"></i>
-      <h2>Button Test</h2>
-      <div style={{width: 500, display: 'flex', gap: '2rem'}}>
-        <Button>Test</Button>
-        <Button
-          outlined
-          onClick={() => {
-            console.log('test');
-          }}
-        >
-          Test outlined
-        </Button>
-
-        <Button
-          onClick={() => {
-            console.log('test');
-          }}
-          disabled
-        >
-          Test disabled
-        </Button>
+    <>
+      <div>
+        <h1>Test icons usage</h1>
+        <i className="icon-chevron-down"></i>
+        <i className="icon-gallery"></i>
+        <i className="icon-logo"></i>
+        <i className="icon-logout"></i>
       </div>
-    </div>
+      <TextField fullWidth size="small" label="Email" type="email" required />
+      <TextField
+        fullWidth
+        size="medium"
+        label="Password"
+        type="password"
+        required
+      />
+      <Button size="large">Test</Button>
+    </>
   );
 };
 
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -6,6 +6,7 @@ const work_sans = Work_Sans({
   style: ['normal'],
   subsets: ['latin'],
 });
+
 export const theme = createTheme({
   palette: {
     primary: {
@@ -36,6 +37,68 @@ export const theme = createTheme({
         },
       },
     },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          '.MuiOutlinedInput-notchedOutline': {
+            borderRadius: 8,
+            borderColor: '#494949',
+          },
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderRadius: 8,
+            borderColor: 'black',
+          },
+          '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#494949',
+          },
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          position: 'relative',
+          legend: {
+            span: {
+              display: 'none',
+            },
+          },
+          '& label': {
+            color: '#494949',
+            position: 'relative',
+            fontSize: 15,
+            fontWeight: 500,
+            transform: 'none',
+            paddingBottom: 8,
+            lineHeight: '17px',
+          },
+          '& label.Mui-focused': {
+            color: '#494949',
+          },
+          '.MuiInputBase-root': {
+            height: 48,
+            fontWeight: 300,
+            fontSize: 15,
+          },
+          '.MuiInputBase-sizeSmall': {
+            height: 34,
+            fontSize: 12,
+          },
+          '& label.MuiInputLabel-sizeSmall': {
+            paddingBottom: 5,
+            lineHeight: '14px',
+            fontSize: 12,
+          },
+        },
+      },
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        asterisk: {
+          color: 'red',
+        },
+      },
+    },
   },
-  typography:{fontFamily:work_sans.style.fontFamily}
+  typography: {fontFamily: work_sans.style.fontFamily},
 });
