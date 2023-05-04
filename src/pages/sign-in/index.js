200719623e54418d62f1a42ef5265f300d1b1a2a
Juan Martín Monasterio <jmonasterio.laba@solvd.com>
2023-05-04 15:19:29 -0300

    add: /sign-in basic structure and style

--- src/pages/sign-in/index.js
+++ src/pages/sign-in/index.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import style from './index.module.css';
+import Button from '@/components/UI/Button/Button';
+import Banner from '@/assets/product8.jpg';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const SignIn = () => {
-  return <div className={style.test}>Sign in</div>;
+  const handleSignIn = () => {
+    console.log("Clicked");
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Sign in</title>
+      </Head>
+      <div className={style.signInContainer}>
+        <main className={style.signInForm}>
+          <div>Welcome back</div>
+          <div>
+            Welcome back! Please enter your details to log into your account.
+          </div>
+          <div>
+            Email <div>Input Email</div>
+          </div>
+          <div>
+            Password <div>Input Password</div>
+          </div>
+          <div>Remember me Forgot Password</div>
+          <Button
+            size="medium"
+            children={'Sign in'}
+            onClick={handleSignIn}
+          />
+          <div>
+            Don't have an account? <Link href="/sign-up">Sign up</Link>
+          </div>
+        </main>
+        <Image src={Banner} alt="banner" width="960" height="1080" />
+      </div>
+    </>
+  );
 };
 
 export default SignIn;
