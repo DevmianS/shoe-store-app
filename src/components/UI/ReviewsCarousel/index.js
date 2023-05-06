f963c3692019fc15804db96256abf34d77bd0b4e
Juan Martín Monasterio <jmonasterio.laba@solvd.com>
2023-05-06 12:33:13 -0300

    init: ReviewsCarousel

---
+++ src/components/UI/ReviewsCarousel/index.js
@@ -0,0 +1,11 @@
+const fakeReviews = [
+  {
+    review:
+      'Lorem Ipsum is a really great company because the team is passionate about the projects they produce, the people they work with, the quality of the work they do.',
+    user: 'John Stone',
+    rating: 5,
+    location: 'Ukraine, Chernivtsi',
+  },
+];
+
+export default function ReviewsCarousel() {}
