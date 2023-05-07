9ec45c03ebd20df1d9008e691e0dd0a114d03abd
Juan Martín Monasterio <jmonasterio.laba@solvd.com>
2023-05-07 14:36:54 -0300

    feat: ReviewsCarousel styling & refactor

--- src/components/UI/ReviewsCarousel/index.js
+++ src/components/UI/ReviewsCarousel/index.js
@@ -2,30 +2,7 @@ import {Box, IconButton, Paper, Rating, Stack, Typography} from '@mui/material';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import {useCallback, useEffect, useRef, useState} from 'react';
-
-const reviews = [
-  {
-    comment:
-      'Lorem Ipsum is a really great company because the team is passionate about the projects they produce, the people they work with, the quality of the work they do.',
-    user: 'John Stone',
-    rating: 5,
-    location: 'Ukraine, Chernivtsi',
-  },
-  {
-    comment:
-      'ValRun is the best company ever because the team is passionate about the projects they produce, the people they work with, the quality of the work they do.',
-    user: 'John Doe',
-    rating: 4.5,
-    location: 'USA, Washington D.C.',
-  },
-  {
-    comment:
-      'Wellrun is not that great of a company because the team is not passionate about the projects they produce, the people they work with, the quality of the work they do.',
-    user: 'Jane Doe',
-    rating: 1.5,
-    location: 'Italy, Roma',
-  },
-];
+import {reviews} from '@/components/UI/ReviewsCarousel/utils';
 
 export default function ReviewsCarousel() {
   const timerRef = useRef(null);
@@ -63,7 +40,7 @@ export default function ReviewsCarousel() {
             'radial-gradient(55.99% 112.1% at 69.71% 44.01%, rgba(253, 253, 253, 0.074) 0%, rgba(0, 0, 0, 0) 100%),radial-gradient(64.9% 185.04% at 19.81% 27.89%, rgba(255, 255, 255, 0.42) 0%, rgba(255, 255, 255, 0.06) 100%)',
           backdropFilter: 'blur(12px)',
           border: '2px solid rgba(255, 255, 255, 0.64)',
-          borderRadius: '32px',
+          borderRadius: 8,
           maxWidth: 756,
           maxHeight: 317,
           p: '2%',
@@ -103,30 +80,37 @@ export default function ReviewsCarousel() {
             {reviews[currentIndex].comment}
           </Typography>
         </Box>
-        <Box>
+        <Box sx={{mt: 2}}>
           <Box
             sx={{
               display: 'flex',
               flexDirection: 'row',
-              justifyContent: 'space-between',
+              justifyContent: 'flex-start',
               alignItems: 'end',
-              width: '50%',
+              minWidth: '50%',
+              columnGap: 3,
             }}
           >
-            <Typography component="span" variant="body4">
+            <Typography
+              component="span"
+              variant="body4"
+              sx={{
+                fontWeight: 700,
+              }}
+            >
               {reviews[currentIndex].user}
             </Typography>
             <Rating
               precision={0.5}
               value={reviews[currentIndex].rating}
-              size="large"
               readOnly
               sx={{
                 color: '#FE645E',
+                columnGap: 2,
               }}
             />
           </Box>
-          <Typography component="p" variant="body2">
+          <Typography component="p" variant="body2" sx={{color: '#797979'}}>
             {reviews[currentIndex].location}
           </Typography>
         </Box>
