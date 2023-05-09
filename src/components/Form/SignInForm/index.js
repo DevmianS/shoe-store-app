2116e1100ae70cc56ae5621fa5355da5c4f38c8e
BrunitoNFT <brunomembrado10@gmail.com>
2023-05-09 15:48:43 -0300

    React Query logic v1

--- src/components/Form/SignInForm/index.js
+++ src/components/Form/SignInForm/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from '@/components/UI/Button';
 import {
   Box,
@@ -9,99 +9,172 @@ import {
   Typography,
 } from '@mui/material';
 
+import {Toaster, toast} from 'sonner';
+
+import {useMutation, useQuery} from '@tanstack/react-query';
+
+import {fetchUtil, registerNewUser, logIn} from '@/utils/utils';
+
+import {useRouter} from 'next/router';
+
+import Spinner from '@/components/UI/Spinner';
+
 const SignInForm = () => {
+  const router = useRouter();
+
   const [rememberMe, setRememberMe] = useState(false);
 
   const handleCheckbox = event => {
     setRememberMe(event.target.checked);
   };
 
-  const handleSignIn = () => {
-    console.log('Clicked');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const {data, isLoading, isError, mutate} = useMutation({
+    mutationKey: ['logIn'],
+    mutationFn: user => logIn(user),
+  });
+
+  const handleSubmit = event => {
+    console.log('handleSubmit');
+    event.preventDefault();
+    const user = {
+      identifier: name,
+      password: password,
+    };
+    mutate(user);
   };
 
+  useEffect(() => {
+    if (localStorage.getItem('user')) {
+      router.push('/');
+    }
+  }, [data, router]);
+
+  const executeError = message => {
+    toast.error(message);
+  };
+
+  useEffect(() => {
+    if (isError) {
+      executeError(
+        "The indentifier and password don't match. Please try again.",
+      );
+    }
+  }, [isError]);
+
   return (
-    <Box
-      sx={{
-        width: '100%',
-        maxWidth: '412px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        flexDirection: 'column',
-        textAlign: 'start',
-        padding: '20px',
-      }}
-    >
-      <Typography component="h1" variant="h3">
-        Welcome back
-      </Typography>
-      <Typography component="p" variant="body1" mb={3}>
-        Welcome back! Please enter your details to log into your account.
-      </Typography>
-      <form>
-        <TextField
-          fullWidth
-          size="medium"
-          label="Email"
-          type="email"
-          margin="normal"
-          required
-        />
-        <TextField
-          fullWidth
-          size="medium"
-          label="Password"
-          type="password"
-          margin="normal"
-          required
-        />
+    <>
+      <Toaster richColors expand={false} position="top-center" />
+      {isLoading && (
         <Box
-          mb={6}
           sx={{
+            width: '100vw',
+            height: '100vh',
+            position: 'fixed',
+            top: '0',
+            left: '0',
             display: 'flex',
-            flexDirection: 'row',
+            justifyContent: 'center',
             alignItems: 'center',
-            justifyContent: 'space-between',
+            background: '#F3F3F3',
+            zIndex: '200',
+            opacity: 0.8,
           }}
         >
-          <FormControlLabel
-            control={<Checkbox onChange={handleCheckbox} />}
-            label="Remember me"
-          />
-          <Link href="/reset-password" underline="none">
-            Forgot password?
-          </Link>
+          <Spinner />
         </Box>
-        <Button size="medium" onClick={handleSignIn}>
-          Sign in
-        </Button>
-      </form>
+      )}
+
       <Box
         sx={{
-          textAlign: 'center',
-          fontSize: 15,
-          textAlign: 'center',
+          width: '100%',
+          maxWidth: '412px',
           display: 'flex',
           justifyContent: 'center',
-          alignItems: 'center',
-          width: '100%',
+          alignItems: 'flex-start',
+          flexDirection: 'column',
+          textAlign: 'start',
+          padding: '20px',
         }}
       >
-        <Typography component="p" sx={{fontWeight: 500, textAlign: 'center'}}>
-          Don’t have an account?
+        <Typography component="h1" variant="h3">
+          Welcome back
         </Typography>
-        <Link
-          href="/sign-up"
-          underline="none"
+        <Typography component="p" variant="body1" mb={3}>
+          Welcome back! Please enter your details to log into your account.
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            size="medium"
+            label="User"
+            type="text"
+            margin="normal"
+            required
+            value={name}
+            onChange={e => setName(e.target.value)}
+            placeholder="Valerii"
+          />
+          <TextField
+            fullWidth
+            size="medium"
+            label="Password"
+            type="password"
+            margin="normal"
+            required
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            placeholder="This_isPassword123"
+          />
+          <Box
+            mb={6}
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <FormControlLabel
+              control={<Checkbox onChange={handleCheckbox} />}
+              label="Remember me"
+            />
+            <Link href="/reset-password" underline="none">
+              Forgot password?
+            </Link>
+          </Box>
+          <Button size="medium" type="submit">
+            Sign in
+          </Button>
+        </form>
+        <Box
           sx={{
-            marginLeft: 1,
+            textAlign: 'center',
+            fontSize: 15,
+            textAlign: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
           }}
         >
-          Sign up
-        </Link>
+          <Typography component="p" sx={{fontWeight: 500, textAlign: 'center'}}>
+            Don’t have an account?
+          </Typography>
+          <Link
+            href="/sign-up"
+            underline="none"
+            sx={{
+              marginLeft: 1,
+            }}
+          >
+            Sign up
+          </Link>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,10 @@ import {ThemeProvider} from '@mui/material';
 import {theme} from '../utils/theme';
 import CssBaseline from '@mui/material/CssBaseline';
 
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 const icons_font = localFont({src: '../font/SHOES_STORE.woff'});
 export default function App({Component, pageProps}) {
   return (
@@ -23,7 +27,9 @@ export default function App({Component, pageProps}) {
         }
       `}</style>
       <CssBaseline />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
