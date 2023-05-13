0a75ebe2ea6e73847351713ed647f385ddd24b29
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-13 22:26:00 +0200

    update card styles

--- src/components/UI/ProductCard/index.js
+++ src/components/UI/ProductCard/index.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
-import Card from '@mui/material/Card';
 import {Typography, Stack, Box, useMediaQuery} from '@mui/material';
-import {styled, useTheme} from '@mui/material/styles';
+import {useTheme} from '@mui/material/styles';
+import {rwdValue} from '@/utils/theme';
 
 export default function ProductCard({title, price, category, imgPath}) {
   const theme = useTheme();
@@ -9,81 +9,89 @@ export default function ProductCard({title, price, category, imgPath}) {
   const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const basis = isMobile ? '50%' : isTablet ? '33.333%' : '25%';
-  const cardMb = isMobile ? '16px' : isTablet ? '15px' : '24px';
-  const cardP = isMobile ? '8px' : isTablet ? '15px' : '24px';
-
   const styles = {
-    position: 'relative',
-    borderRadius: 0,
-    border: 'none',
-    boxShadow: 'none',
-    '& img': {
-      position: 'absolute',
-      background: 'primary',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      objectFit: 'cover',
-      transition: '1s',
-    },
-    '&:hover': isDesktop
+    column: isMobile
       ? {
-          cursor: 'pointer',
-          '& img': {
-            transition: '1s',
-            transform: 'scale(1.25)',
-          },
+          flex: '0 0 50%',
+          padding: '0 8px',
+          marginBottom: '16px',
         }
-      : {},
+      : isTablet
+      ? {
+          flex: '0 0 33.333%',
+          padding: '0 15px',
+          marginBottom: '15px',
+        }
+      : {
+          flex: '0 0 25%',
+          padding: '0 24px',
+          marginBottom: '24px',
+        },
+    card: {
+      position: 'relative',
+      borderRadius: 0,
+      border: 'none',
+      boxShadow: 'none',
+      '& img': {
+        position: 'absolute',
+        background: 'primary',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        transition: '1s',
+      },
+      '&:hover': isDesktop
+        ? {
+            cursor: 'pointer',
+            '& img': {
+              transition: '1s',
+              transform: 'scale(1.25)',
+            },
+          }
+        : {},
+    },
+    image: {
+      position: 'relative',
+      width: '100%',
+      paddingBottom: '120%',
+      marginBottom: '12px',
+      overflow: 'hidden',
+      background: 'lightgrey',
+    },
+    body: {position: 'relative'},
+    header: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    title: {
+      fontSize: rwdValue(10, 22),
+      fontWeight: 500,
+    },
+    category: {
+      fontSize: rwdValue(8, 18),
+      color: theme.palette.text.secondary,
+    },
   };
 
   return (
-    <Box
-      sx={{flex: `0 0 ${basis}`, padding: `0 ${cardP}`, marginBottom: cardMb}}
-    >
-      <Box sx={styles}>
-        <Box
-          sx={{
-            position: 'relative',
-            width: '100%',
-            paddingBottom: '120%',
-            marginBottom: '12px',
-            overflow: 'hidden',
-            background: 'lightgrey',
-          }}
-        >
+    <Box sx={styles.column}>
+      <Box sx={styles.card}>
+        <Box sx={styles.image}>
           <Image src={imgPath} alt={`${title} ${category}`} />
         </Box>
-        <Box sx={{position: 'relative'}}>
-          <Stack
-            flexDirection={'row'}
-            justifyContent={'space-between'}
-            alignItems={'center'}
-          >
-            <Typography
-              component="h3"
-              fontSize="calc(10px + 12 * ((100vw - 360px) / (1920 - 360)))"
-              fontWeight={500}
-            >
+        <Box sx={styles.body}>
+          <Stack sx={styles.header}>
+            <Typography component="h3" sx={styles.title}>
               {title || 'Product title'}
             </Typography>
-            <Typography
-              component="span"
-              fontSize="calc(10px + 12 * ((100vw - 360px) / (1920 - 360)))"
-              fontWeight={500}
-            >
+            <Typography component="span" sx={styles.title}>
               ${price || '100'}
             </Typography>
           </Stack>
-          <Typography
-            sx={{
-              fontSize: 'calc(8px + 10 * ((100vw - 360px) / (1920 - 360)))',
-              color: '#5C5C5C',
-            }}
-            component="h4"
-          >
+          <Typography sx={styles.category} component="h4">
             {category || 'category'}
           </Typography>
         </Box>
