abe421e903ed3c1953556b56466ebe8114ff64fd
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-15 19:12:48 +0200

    update sidebar, remove unused imports

--- src/components/Layout/SideBar/index.js
+++ src/components/Layout/SideBar/index.js
@@ -1,11 +1,35 @@
-import {Stack, Box} from '@mui/material';
+import {Stack, Box, List} from '@mui/material';
 import useOwnStyles from '@/utils/styles';
+import {useRouter} from 'next/router';
+import ListItem from '@/components/UI/ListItem';
+import AvatarStaticLayout from '../AvatarStaticLayout';
 
 export default function SideBar({children}) {
+  const router = useRouter();
   const {sideBar: styles} = useOwnStyles();
+
   return (
     <Box sx={styles}>
-      <Stack aria-label="user actions">{children}</Stack>
+      <Stack aria-label="user actions">
+        <AvatarStaticLayout />
+        <List>
+          <ListItem
+            name="My products"
+            icon="bag-o"
+            onClick={() => router.push('/my-products')}
+          />
+          <ListItem
+            name="Settings"
+            icon="setting"
+            onClick={() => router.push('/profile/update')}
+          />
+          <ListItem
+            name="Log-out"
+            icon="logout"
+            onClick={() => router.push('/profile/update')}
+          />
+        </List>
+      </Stack>
     </Box>
   );
 }
