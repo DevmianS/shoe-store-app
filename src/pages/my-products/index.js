78ac775c023a50080d229795398d8dc9ffeb1de3
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-16 00:32:32 +0200

    move styles to useStyle

--- src/pages/my-products/index.js
+++ src/pages/my-products/index.js
@@ -1,78 +1,44 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 import {Typography, Box, Stack} from '@mui/material';
 
-import {rwdValue} from '@/utils/theme';
-import mockupProducts from '@/utils/data';
+import useOwnStyles from '@/utils/styles';
+import bannerImg from '@/assets/banner.jpg';
 
 import SideBar from '@/components/Layout/SideBar';
 import NavBarLayout from '@/components/Layout/NavBarLayout';
 
 import ProductCard from '@/components/UI/ProductCard';
-import bannerImg from '@/assets/banner.jpg';
 import TopBanner from '@/components/UI/TopBanner';
-
 import AvatarStatic from '@/components/UI/AvatarStatic';
 import Button from '@/components/UI/Button';
-import Link from 'next/link';
 
 const MyProducts = ({userName, items}) => {
+  const {myProducts: styles} = useOwnStyles();
   return (
     <>
       <Head>
         <title>Wellrun | My Products</title>
       </Head>
       <NavBarLayout>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            paddingTop: rwdValue(0, 40),
-            paddingBottom: rwdValue(0, 40),
-          }}
-        >
+        <Box sx={styles.row}>
           <SideBar />
-          <Box
-            sx={{
-              flex: '1 1 auto',
-              padding: {lg: '0 60px', md: '0 40px', sm: '0 30px', xs: '0 10px'},
-            }}
-          >
+          <Box sx={styles.header}>
             <TopBanner imgPath={bannerImg.src} />
-            <Stack
-              direction="row"
-              alignItems="end"
-              sx={{
-                ml: {xl: '70px', md: '50px', sm: '30px', xs: '30px'},
-                mb: '50px',
-                mt: '-30px',
-              }}
-            >
-              <AvatarStatic
-                variant="medium"
-                sx={{
-                  marginRight: rwdValue(5, 15),
-                  border: '4px solid white',
-                  zIndex: 2,
-                }}
-              />
+            <Stack sx={styles.avatarWrapper}>
+              <AvatarStatic variant="medium" sx={styles.avatar} />
               <Box>
-                <Typography variant="body2" component="h3">
+                <Typography variant="body2" component="h3" sx={styles.name}>
                   {userName || 'Jane Meldrum'}
                 </Typography>
-                <Typography color="text.tetriary" fontSize={15} mb="12px">
-                  1 374 bonus points
-                </Typography>
+                <Typography sx={styles.points}>1 374 bonus points</Typography>
               </Box>
             </Stack>
-            <Typography variant="h1" component="h1" mb="40px">
+            <Typography variant="h1" component="h1" sx={styles.h1}>
               My products
             </Typography>
-            <Box
-              display="flex"
-              flexWrap="wrap"
-              margin={{sm: '0 -8px', md: '0 -24px'}}
-            >
+            <Box sx={styles.productsRow}>
               {items &&
                 items.length > 0 &&
                 items.map(product => {
@@ -87,42 +53,23 @@ const MyProducts = ({userName, items}) => {
                   );
                 })}
               {!items && (
-                <Box
-                  sx={{maxWidth: 320, textAlign: 'center', margin: '0 auto'}}
-                >
+                <Box sx={styles.msgBody}>
                   <Typography
                     className="icon-bag-o"
-                    sx={{
-                      fontSize: 20,
-                      width: 72,
-                      height: 72,
-                      borderRadius: '50%',
-                      background: '#F9FAFB',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      margin: '0 auto 10px',
-                    }}
+                    sx={styles.msgIcon}
                   ></Typography>
                   <Typography
                     component="h2"
                     variant="body2"
-                    sx={{fontSize: rwdValue(16, 20), marginBottom: '10px'}}
+                    sx={styles.msgTitle}
                   >
                     You don’t have any products yet
                   </Typography>
-                  <Typography
-                    component="p"
-                    variant="body1"
-                    sx={{
-                      fontSize: rwdValue(12, 15),
-                      marginBottom: rwdValue(32, 40),
-                    }}
-                  >
+                  <Typography component="p" variant="body1" sx={styles.msgText}>
                     Post can contain video, images and text.
                   </Typography>
                   <Link href="/add-product">
-                    <Button sx={{maxWidth: '152px'}}>Add product</Button>
+                    <Button sx={styles.msgBtn}>Add product</Button>
                   </Link>
                 </Box>
               )}
--- src/utils/styles.js
+++ src/utils/styles.js
@@ -7,6 +7,8 @@ const useOwnStyles = () => {
   const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
+  // ====PAGES====
+  // /profile/update
   const updateProfile = {
     row: {
       display: 'flex',
@@ -54,6 +56,63 @@ const useOwnStyles = () => {
     item: {marginBottom: rwdValue(25, 50)},
     size: isMobile ? 'small' : 'medium',
   };
+  // /my-products
+  const myProducts = {
+    row: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      paddingTop: rwdValue(0, 40),
+      paddingBottom: rwdValue(0, 40),
+    },
+    header: {
+      flex: '1 1 auto',
+      paddingLeft: rwdValue(10, 60),
+      paddingRight: rwdValue(10, 60),
+    },
+    avatarWrapper: {
+      marginLeft: rwdValue(20, 50),
+      marginBottom: rwdValue(20, 30),
+      flexDirection: 'row',
+      alignItems: 'end',
+      marginTop: isMobile || isTablet ? '-15px' : '-30px',
+    },
+    avatar: {
+      marginRight: rwdValue(5, 15),
+      border: '4px solid white',
+      zIndex: 2,
+    },
+    name: {fontSize: rwdValue(14, 20)},
+    points: {
+      fontSize: rwdValue(12, 15),
+      color: theme.palette.text.tetriary,
+      marginBottom: isMobile ? 0 : rwdValue(0, 12),
+    },
+    h1: {marginBottom: rwdValue(20, 35)},
+    productsRow: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      margin: isMobile ? '0 -8px' : '0 -24px',
+    },
+    msgBody: {maxWidth: '320px', textAlign: 'center', margin: '0 auto'},
+    msgIcon: {
+      fontSize: 20,
+      width: 72,
+      height: 72,
+      borderRadius: '50%',
+      background: '#F9FAFB',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: '0 auto 10px',
+    },
+    msgTitle: {fontSize: rwdValue(16, 20), marginBottom: '10px'},
+    msgText: {
+      fontSize: rwdValue(12, 15),
+      marginBottom: rwdValue(32, 40),
+    },
+    msgBtn: {maxWidth: '152px'},
+  };
+
   const sideBar = {
     position: isTablet || isMobile ? 'fixed' : 'static',
     maxWidth: isTablet || isMobile ? '270px' : '320px',
@@ -67,6 +126,7 @@ const useOwnStyles = () => {
     zIndex: 5,
     transform: isTablet || isMobile ? 'translateX(100%)' : 'none',
   };
-  return {updateProfile, sideBar};
+
+  return {updateProfile, sideBar, myProducts};
 };
 export default useOwnStyles;
