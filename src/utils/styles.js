f215302b5729b03786a1d37e89a5fe57089bf850
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-16 10:48:48 +0200

    move rootBanner styles

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -72,6 +72,44 @@ const useOwnStyles = () => {
         filter: 'brightness(1.1)',
       },
     },
+    rootBanner: {
+      column: {
+        width: '50%',
+        height: '100vh',
+        overflow: 'hidden',
+        position: 'relative',
+        display: {
+          xs: 'none',
+          md: 'flex',
+        },
+      },
+      image: {
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        position: 'absolute',
+        zIndex: '-1',
+      },
+      right: {
+        zIndex: '100',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+      },
+      rightTop: {
+        width: '100%',
+        height: '50%',
+      },
+      rightBot: {
+        width: '100%',
+        height: '50%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'flex-start',
+        padding: 1,
+      },
+    },
   };
   return {updateProfile, sideBar, UI};
 };
