1417e24c73d7a6c07cd14d82e5e570aa26379c4c
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-18 23:27:00 +0200

    update spinenr

--- src/components/UI/Loading/index.js
+++ src/components/UI/Loading/index.js
@@ -3,22 +3,21 @@ import Spinner from '../Spinner';
 import {Box} from '@mui/material';
 
 const Loading = () => {
+  const styles = {
+    width: '100vw',
+    height: '100vh',
+    position: 'fixed',
+    top: '0',
+    left: '0',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: '#F3F3F3',
+    zIndex: '200',
+    opacity: 0.8,
+  };
   return (
-    <Box
-      sx={{
-        width: '100vw',
-        height: '100vh',
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        background: '#F3F3F3',
-        zIndex: '200',
-        opacity: 0.8,
-      }}
-    >
+    <Box sx={styles}>
       <Spinner />
     </Box>
   );
