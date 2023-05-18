6bd227d8fd4b98e23d47c435a015a35e82a840d1
Juan Martín Monasterio <jmonasterio.laba@solvd.com>
2023-05-18 16:48:06 -0300

    fix: added deleted code

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -3,13 +3,28 @@ import {useSession} from 'next-auth/react';
 const useUser = () => {
   const {data, status} = useSession();
 
-  const username = data?.user?.user?.username;
+  const user = data?.user?.user;
+
+  let username = null;
+  if (user?.firstName && user?.lastName) {
+    username = `${user.firstName} ${user.lastName}`;
+  } else {
+    username = user?.username;
+  }
+
+  let userInitials = null;
+  if (username?.includes(' ')) {
+    const [firstName, lastName] = username?.split(' ');
+    userInitials = `${firstName[0]}${lastName[0]}`.toLocaleUpperCase();
+  } else {
+    userInitials = username?.substring(0, 2).toLocaleUpperCase();
+  }
 
   return {
     name: username || '',
     status,
     data,
-    initials: (username && username.substring(0, 2).toUpperCase()) || '',
+    initials: userInitials || '',
   };
 };
 
