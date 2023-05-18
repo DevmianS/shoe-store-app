780a9072f891d741e99be1817edc17ebf4606ee4
DevmianS <damiansobierajdev@gmail.com>
2023-05-18 18:09:48 +0200

    fix: moved styled wrapper of navbarLayout, outside of the component to prevent unnecessary rerenders

--- src/components/Layout/NavBarLayout/index.js
+++ src/components/Layout/NavBarLayout/index.js
@@ -2,23 +2,23 @@ import React, {memo} from 'react';
 import NavBar from '@/components/UI/NavBar';
 import {Box, styled} from '@mui/material';
 
-const NavBarLayout = ({children}) => {
-  const Wrapper = styled(Box)({
-    position: 'relative',
+const Wrapper = styled(Box)({
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  height: '100%',
+  minHeight: '100vh',
+  '& main': {
+    flex: '1 1 auto',
     display: 'flex',
     flexDirection: 'column',
-    width: '100%',
-    height: '100%',
-    minHeight: '100vh',
-    '& main': {
+    '& > *': {
       flex: '1 1 auto',
-      display: 'flex',
-      flexDirection: 'column',
-      '& > *': {
-        flex: '1 1 auto',
-      },
     },
-  });
+  },
+});
+const NavBarLayout = ({children}) => {
   return (
     <Wrapper key="wrap">
       <NavBar key="nav" />
