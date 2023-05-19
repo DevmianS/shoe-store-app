1da4c18155c7d57161ba4d630db182def2fefb9f
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-19 23:40:33 +0200

    update count select

--- src/components/UI/CartProductItem/index.js
+++ src/components/UI/CartProductItem/index.js
@@ -145,11 +145,15 @@ export default function CartProductItem({
                 <MenuItem value="quantity" disabled>
                   Quantity
                 </MenuItem>
-                <MenuItem value={1}>1</MenuItem>
-                <MenuItem value={2}>2</MenuItem>
-                <MenuItem value={3}>3</MenuItem>
-                <MenuItem value={4}>4</MenuItem>
-                <MenuItem value={5}>5</MenuItem>
+                {[...Array(10)].map((_, index) => (
+                  <MenuItem
+                    key={index}
+                    value={index + 1}
+                    defaultChecked={quantity === index + 1}
+                  >
+                    {index + 1}
+                  </MenuItem>
+                ))}
               </Select>
             </FormControl>
             <Box sx={styles.delete} onClick={onDelete}>
