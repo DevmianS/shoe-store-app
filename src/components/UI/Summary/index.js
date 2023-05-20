fe25dba5be3a9e0d71541922b767bc0d8e1ee08d
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-20 12:09:43 +0200

    split summary component

---
+++ src/components/UI/Summary/index.js
@@ -0,0 +1,126 @@
+import {useState, useEffect} from 'react';
+
+import {Typography, Box, Stack, useMediaQuery, useTheme} from '@mui/material';
+
+import {rwdValue} from '@/utils/theme';
+
+import Button from '@/components/UI/Button';
+
+export default function Summary({items, cartItems}) {
+  const [cost, setCost] = useState({
+    shipping: 0,
+    subtotal: 0,
+    total: 0,
+    tax: 0,
+  });
+
+  useEffect(() => {
+    const subtotal = Number(
+      items.reduce(
+        (acc, prod) =>
+          prod.attributes.price * cartItems[prod.attributes.name] + acc,
+        0,
+      ),
+    ).toFixed(2);
+    const shipping = Number(subtotal / 10).toFixed(2);
+    const tax = Number((subtotal / 100) * 19).toFixed(2);
+    const total = Number(+shipping + +tax + +subtotal).toFixed(2);
+    setCost({
+      shipping,
+      tax,
+      total,
+      subtotal,
+    });
+  }, [items]);
+
+  const theme = useTheme();
+  const isTablet = useMediaQuery(theme.breakpoints.down('md'));
+
+  const styles = {
+    colSm: {flex: isTablet ? '0 0 100%' : '0 0 26%'},
+    promo: {
+      fontWeight: 400,
+      marginBottom: rwdValue(20, 40),
+      display: 'flex',
+      alignItems: 'center',
+      '& i': {fontSize: 20, marginLeft: '5px'},
+    },
+    summaryTitle: {
+      marginBottom: rwdValue(20, 55),
+      paddingBottom: isTablet ? rwdValue(20, 40) : 0,
+      borderBottom: isTablet ? '1px solid #EAECF0' : 'none',
+    },
+    summaryText: {fontSize: rwdValue(20, 30), fontWeight: 400},
+    summaryTotal: {
+      display: 'flex',
+      flexDirection: 'row',
+      padding: '20px 0',
+      marginBottom: rwdValue(30, 55),
+      marginTop: rwdValue(30, 55),
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      borderTop: '1px solid #EAECF0',
+      borderBottom: '1px solid #EAECF0',
+      fontSize: rwdValue(20, 30),
+      fontWeight: 500,
+    },
+    summaryItem: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      marginBottom: rwdValue(20, 40),
+    },
+  };
+
+  return (
+    <Box sx={styles.colSm}>
+      <Typography variant="h1" component="h2" sx={styles.summaryTitle}>
+        Summary
+      </Typography>
+      <Box>
+        <Typography variant="body2" component="p" sx={styles.promo}>
+          Do you have a promocode?
+          <Typography className="icon-chevron-down" component="i" />
+        </Typography>
+        <Stack sx={styles.summaryItem}>
+          <Typography variant="h3" component="h3" sx={styles.summaryText}>
+            Subtotal
+          </Typography>
+          <Typography variant="h3" component="span" sx={styles.summaryText}>
+            ${cost.subtotal}
+          </Typography>
+        </Stack>
+        <Stack sx={styles.summaryItem}>
+          <Typography variant="h3" component="h3" sx={styles.summaryText}>
+            Shipping
+          </Typography>
+          <Typography variant="h3" component="span" sx={styles.summaryText}>
+            ${cost.shipping}
+          </Typography>
+        </Stack>
+        <Stack sx={styles.summaryItem}>
+          <Typography variant="h3" component="h3" sx={styles.summaryText}>
+            Tax
+          </Typography>
+          <Typography variant="h3" component="span" sx={styles.summaryText}>
+            ${cost.tax}
+          </Typography>
+        </Stack>
+        <Stack sx={styles.summaryTotal}>
+          <Typography variant="h3" component="h3">
+            Total
+          </Typography>
+          <Typography
+            variant="h3"
+            component="span"
+            sx={{...styles.summaryText, fontWeight: 500}}
+          >
+            ${cost.total}
+          </Typography>
+        </Stack>
+        <Button size={isTablet ? 'small' : 'medium'}>Checkout</Button>
+      </Box>
+    </Box>
+  );
+}
--- src/pages/bag/index.js
+++ src/pages/bag/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import {useState, useEffect} from 'react';
 
-import {Typography, Box, Stack, useMediaQuery, useTheme} from '@mui/material';
+import {Typography, Box, useMediaQuery, useTheme} from '@mui/material';
 
 import {rwdValue} from '@/utils/theme';
 import {SkeletonProducts} from '@/utils/utils';
@@ -15,36 +15,13 @@ import SideBar from '@/components/Layout/SideBar';
 
 import Button from '@/components/UI/Button';
 import CartProductItem from '@/components/UI/CartProductItem';
+import Summary from '@/components/UI/Summary';
 
 const Bag = () => {
   const {products, isLoading} = useProducts();
   const [items, setItems] = useState([]);
   const {cartItems} = useCart();
-  const [cost, setCost] = useState({
-    shipping: 0,
-    subtotal: 0,
-    total: 0,
-    tax: 0,
-  });
 
-  useEffect(() => {
-    const subtotal = Number(
-      items.reduce(
-        (acc, prod) =>
-          prod.attributes.price * cartItems[prod.attributes.name] + acc,
-        0,
-      ),
-    ).toFixed(2);
-    const shipping = Number(subtotal / 10).toFixed(2);
-    const tax = Number((subtotal / 100) * 19).toFixed(2);
-    const total = Number(+shipping + +tax + +subtotal).toFixed(2);
-    setCost({
-      shipping,
-      tax,
-      total,
-      subtotal,
-    });
-  }, [cartItems, items]);
   useEffect(() => {
     const keys = Object.keys(cartItems);
 
@@ -68,11 +45,6 @@ const Bag = () => {
       fontSize: rwdValue(18, 28),
       marginTop: rwdValue(45, 65),
       textAlign: 'center',
-    },
-    summaryTitle: {
-      marginBottom: rwdValue(20, 55),
-      paddingBottom: isTablet ? rwdValue(20, 40) : 0,
-      borderBottom: isTablet ? '1px solid #EAECF0' : 'none',
     },
     btnBox: {
       textAlign: 'center',
@@ -108,35 +80,6 @@ const Bag = () => {
       flexWrap: 'wrap',
     },
     colBig: {flex: isTablet ? '0 0 100%' : '0 1 62%'},
-    colSm: {flex: isTablet ? '0 0 100%' : '0 0 26%'},
-    promo: {
-      fontWeight: 400,
-      marginBottom: rwdValue(20, 40),
-      display: 'flex',
-      alignItems: 'center',
-      '& i': {fontSize: 20, marginLeft: '5px'},
-    },
-    summaryText: {fontSize: rwdValue(20, 30), fontWeight: 400},
-    total: {
-      display: 'flex',
-      flexDirection: 'row',
-      padding: '20px 0',
-      marginBottom: rwdValue(30, 55),
-      marginTop: rwdValue(30, 55),
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      borderTop: '1px solid #EAECF0',
-      borderBottom: '1px solid #EAECF0',
-      fontSize: rwdValue(20, 30),
-      fontWeight: 500,
-    },
-    summaryItem: {
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      marginBottom: rwdValue(20, 40),
-    },
   };
   return (
     <>
@@ -189,78 +132,7 @@ const Bag = () => {
                 </>
               )}
             </Box>
-            <Box sx={styles.colSm}>
-              <Typography variant="h1" component="h2" sx={styles.summaryTitle}>
-                Summary
-              </Typography>
-              <Box>
-                <Typography variant="body2" component="p" sx={styles.promo}>
-                  Do you have a promocode?
-                  <Typography className="icon-chevron-down" component="i" />
-                </Typography>
-                <Stack sx={styles.summaryItem}>
-                  <Typography
-                    variant="h3"
-                    component="h3"
-                    sx={styles.summaryText}
-                  >
-                    Subtotal
-                  </Typography>
-                  <Typography
-                    variant="h3"
-                    component="span"
-                    sx={styles.summaryText}
-                  >
-                    ${cost.subtotal}
-                  </Typography>
-                </Stack>
-                <Stack sx={styles.summaryItem}>
-                  <Typography
-                    variant="h3"
-                    component="h3"
-                    sx={styles.summaryText}
-                  >
-                    Shipping
-                  </Typography>
-                  <Typography
-                    variant="h3"
-                    component="span"
-                    sx={styles.summaryText}
-                  >
-                    ${cost.shipping}
-                  </Typography>
-                </Stack>
-                <Stack sx={styles.summaryItem}>
-                  <Typography
-                    variant="h3"
-                    component="h3"
-                    sx={styles.summaryText}
-                  >
-                    Tax
-                  </Typography>
-                  <Typography
-                    variant="h3"
-                    component="span"
-                    sx={styles.summaryText}
-                  >
-                    ${cost.tax}
-                  </Typography>
-                </Stack>
-                <Stack sx={styles.total}>
-                  <Typography variant="h3" component="h3">
-                    Total
-                  </Typography>
-                  <Typography
-                    variant="h3"
-                    component="span"
-                    sx={{...styles.summaryText, fontWeight: 500}}
-                  >
-                    ${cost.total}
-                  </Typography>
-                </Stack>
-                <Button size={isTablet ? 'small' : 'medium'}>Checkout</Button>
-              </Box>
-            </Box>
+            <Summary items={items} cartItems={cartItems} />
           </Box>
         </Box>
       </NavBarLayout>
