dd429e72537f2e90017f9827c65cba02530f627c
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-22 09:42:01 +0200

    split styles

--- src/components/Layout/Error/index.js
+++ src/components/Layout/Error/index.js
@@ -11,74 +11,65 @@ export default function ErrorLayout({image, title, text}) {
   const theme = useTheme();
   const isTablet = useMediaQuery(theme.breakpoints.down('md'));
 
-  const styles = {};
+  const styles = {
+    row: {
+      display: 'flex',
+      flexDirection: isTablet ? 'column-reverse' : 'row',
+      justifyContent: isTablet ? 'flex-end' : 'flex-start',
+      minHeight: '100%',
+    },
+    textCol: {
+      flex: isTablet ? '0 0 100%' : '0 0 50%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textContent: {padding: '30px', maxWidth: '480px'},
+    title: {
+      marginBottom: rwdValue(10, 20),
+      textAlign: isTablet ? 'center' : 'start',
+    },
+    text: {
+      marginBottom: rwdValue(30, 40),
+      fontSize: rwdValue(15, 20),
+    },
+    link: {
+      display: 'flex',
+      justifyContent: isTablet ? 'center' : 'start',
+    },
+    btn: {maxWidth: isTablet ? '180px' : '280px'},
+    btnSize: isTablet ? 'small' : 'medium',
+    imageCol: {
+      flex: isTablet ? '0 0 100%' : '0 0 50%',
+      display: 'flex',
+      alignItems: 'start',
+      padding: '0 30px',
+      '& img': {margin: '0 auto', maxWidth: '100%', height: 'auto'},
+    },
+    imgSize: isTablet ? 500 : 700,
+  };
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: isTablet ? 'column-reverse' : 'row',
-        justifyContent: isTablet ? 'flex-end' : 'flex-start',
-        minHeight: '100%',
-      }}
-    >
-      <Box
-        sx={{
-          flex: isTablet ? '0 0 100%' : '0 0 50%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Box sx={{padding: '30px', maxWidth: '480px'}}>
-          <Typography
-            component="h1"
-            variant="h1"
-            sx={{
-              marginBottom: rwdValue(10, 20),
-              textAlign: isTablet ? 'center' : 'start',
-            }}
-          >
+    <Box sx={styles.row}>
+      <Box sx={styles.textCol}>
+        <Box sx={styles.textContent}>
+          <Typography component="h1" variant="h1" sx={styles.title}>
             {title}
           </Typography>
-          <Typography
-            component="p"
-            variant="body1"
-            sx={{
-              marginBottom: rwdValue(30, 40),
-              fontSize: rwdValue(15, 20),
-            }}
-          >
+          <Typography component="p" variant="body1" sx={styles.text}>
             {text}
           </Typography>
-          <Link
-            href="/"
-            style={{
-              display: 'flex',
-              justifyContent: isTablet ? 'center' : 'start',
-            }}
-          >
-            <Button
-              sx={{maxWidth: isTablet ? '180px' : '280px'}}
-              size={isTablet ? 'small' : 'medium'}
-            >
+          <Link href="/" style={styles.link}>
+            <Button sx={styles.btn} size={styles.btnSize}>
               Back home
             </Button>
           </Link>
         </Box>
       </Box>
-      <Box
-        sx={{
-          flex: isTablet ? '0 0 100%' : '0 0 50%',
-          display: 'flex',
-          alignItems: 'start',
-          padding: '0 30px',
-          '& img': {margin: '0 auto', maxWidth: '100%', height: 'auto'},
-        }}
-      >
+      <Box sx={styles.imageCol}>
         <Image
           src={image}
-          width={isTablet ? 500 : 700}
-          height={isTablet ? 500 : 700}
+          width={styles.imgSize}
+          height={styles.imgSize}
         ></Image>
       </Box>
     </Box>
