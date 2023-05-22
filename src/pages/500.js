00ad12a14464081a63c849a4be1c9a64c53cef0f
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-22 09:54:56 +0200

    add error 500 page

--- src/pages/500.js
+++ src/pages/500.js
@@ -1,15 +1,20 @@
 import Head from 'next/head';
 
+import NavBarLayout from '@/components/Layout/NavBarLayout';
+import ErrorLayout from '@/components/Layout/Error';
+
 function ErrorPage500() {
-  const pageTitle = 'We lost that page...!';
-  const msgText = `We're sorry, but the page you're looking for is currently unavailable. Our team has been notified of the issue and is working to resolve it as quickly as possible. In the meantime, please try again later or contact us if you continue to experience this problem. Thank you for your patience`;
+  const title = 'We lost that page...!';
+  const message = `We're sorry, but the page you're looking for is currently unavailable. Our team has been notified of the issue and is working to resolve it as quickly as possible. In the meantime, please try again later or contact us if you continue to experience this problem. Thank you for your patience`;
 
   return (
     <>
       <Head>
-        <title>Wellrun | Server Error</title>
+        <title>Wellrun | 500 Error</title>
       </Head>
-      505
+      <NavBarLayout>
+        <ErrorLayout title={title} text={message} image={'/500.png'} />
+      </NavBarLayout>
     </>
   );
 }
