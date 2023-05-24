dc0f59fca0e0ff9b0d999aacc7de6c61fbfd4d59
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-25 00:14:15 +0200

    update rootform styles

--- src/components/Form/RootForm/RootForm.js
+++ src/components/Form/RootForm/RootForm.js
@@ -1,54 +1,60 @@
-import Button from '@/components/UI/Button';
-import {rwdValue} from '@/utils/theme';
-import {Box, Typography, useMediaQuery, useTheme} from '@mui/material';
-
 import {useRouter} from 'next/router';
 
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+import {rwdValue} from '@/utils/theme';
+import {theme} from '@/utils/theme';
+
+import Button from '@/components/UI/Button';
+
+const rootFormStyles = {
+  wrapper: {
+    width: '100%',
+    maxWidth: '480px',
+  },
+  welcomeTitle: {marginBottom: '10px'},
+  welcomeText: {
+    fontSize: rwdValue(10, 15),
+    color: theme.palette.text.secondary,
+    marginBottom: rwdValue(15, 25),
+  },
+  signInBtn: {
+    marginBottom: rwdValue(15, 25),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  signUpBtn: {
+    color: 'white',
+  },
+};
+
 const RootForm = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-
-  const styles = {
-    wrapper: {
-      width: '100%',
-      maxWidth: isMobile ? '100%' : '480px',
-    },
-    welcomeTitle: {marginBottom: '10px'},
-    welcomeText: {
-      fontSize: rwdValue(10, 15),
-      color: theme.palette.text.secondary,
-      marginBottom: isMobile ? '15px' : '25px',
-    },
-    signInBtn: {
-      marginBottom: isMobile ? '15px' : '25px',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    signUpBtn: {
-      color: 'white',
-    },
-  };
-
   const router = useRouter();
   return (
-    <Box sx={styles.wrapper}>
-      <Typography component="h1" variant="h1" sx={styles.welcomeTitle}>
+    <Box sx={rootFormStyles.wrapper}>
+      <Typography component="h1" variant="h1" sx={rootFormStyles.welcomeTitle}>
         Welcome to Wellrun
       </Typography>
-      <Typography component="p" variant="body1" sx={styles.welcomeText}>
+      <Typography component="p" variant="body1" sx={rootFormStyles.welcomeText}>
         The biggest shoes business in the whole world.
       </Typography>
 
       <Box>
         <Button
           onClick={() => router.push('/sign-in')}
-          sx={styles.signInBtn}
+          sx={rootFormStyles.signInBtn}
           outlined={true}
         >
           Sign in
         </Button>
-        <Button onClick={() => router.push('/sign-up')} sx={styles.signUpBtn}>Sign up</Button>
+        <Button
+          onClick={() => router.push('/sign-up')}
+          sx={rootFormStyles.signUpBtn}
+        >
+          Sign up
+        </Button>
       </Box>
     </Box>
   );
