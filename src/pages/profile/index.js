f8f51dd99a881c8c03e650d9d8a57f91c571c17c
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-25 01:09:34 +0200

    update profile styles

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,21 +1,43 @@
 import Head from 'next/head';
-import {getServerSession} from 'next-auth/next';
 
-import {Typography, Box, Stack, Skeleton} from '@mui/material';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 import {rwdValue} from '@/utils/theme';
+import {SkeletonProducts} from '@/utils/utils';
+import useProducts from '@/hooks/useProducts';
 
 import SideBar from '@/components/Layout/SideBar';
 import NavBarLayout from '@/components/Layout/NavBarLayout';
+import AvatarStaticLayout from '@/components/Layout/AvatarStaticLayout';
 
 import ProductCard from '@/components/UI/ProductCard';
-
-import bannerImg from '@/assets/banner.jpg';
 import TopBanner from '@/components/UI/TopBanner';
 
-import useProducts from '@/hooks/useProducts';
-import AvatarStaticLayout from '@/components/Layout/AvatarStaticLayout';
-import {SkeletonProducts} from '@/utils/utils';
+import bannerImg from '@/assets/banner.jpg';
+
+const profileStyles = {
+  row: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingTop: rwdValue(0, 40),
+    paddingBottom: rwdValue(0, 40),
+  },
+  col: {
+    flex: '1 1 auto',
+    padding: {
+      lg: '0 60px',
+      md: '0 40px',
+      sm: '0 30px',
+      xs: '0 10px',
+    },
+  },
+  prod: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    margin: {sm: '0 -8px', md: '0 -24px'},
+  },
+};
 
 const Profile = () => {
   const {data, isLoading} = useProducts();
@@ -27,36 +49,15 @@ const Profile = () => {
         <title>Wellrun | My Profile</title>
       </Head>
       <NavBarLayout>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            paddingTop: rwdValue(0, 40),
-            paddingBottom: rwdValue(0, 40),
-          }}
-        >
+        <Box sx={profileStyles.row}>
           <SideBar />
-          <Box
-            sx={{
-              flex: '1 1 auto',
-              padding: {
-                lg: '0 60px',
-                md: '0 40px',
-                sm: '0 30px',
-                xs: '0 10px',
-              },
-            }}
-          >
+          <Box sx={profileStyles.col}>
             <TopBanner imgPath={bannerImg.src} />
             <AvatarStaticLayout />
             <Typography variant="h1" component="h1" mb="40px">
               Last viewed products
             </Typography>
-            <Box
-              display="flex"
-              flexWrap="wrap"
-              margin={{sm: '0 -8px', md: '0 -24px'}}
-            >
+            <Box sx={profileStyles.prod}>
               {isLoading && SkeletonProducts()}
 
               {products &&
