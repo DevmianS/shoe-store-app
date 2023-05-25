e503d4394bf56c19a9dfe43026690b671e8ff760
BrunitoNFT <brunomembrado10@gmail.com>
2023-05-25 09:05:18 -0300

    Small fix

--- src/context/FilterContext.js
+++ src/context/FilterContext.js
@@ -58,7 +58,10 @@ export const FilterProvider = ({children}) => {
     );
     console.log('Query: ', query);
 
-    router.push('search?' + query);
+    if (query) {
+      console.log("Passed")
+      router.push('search?' + query);
+    }
   }
 
   useEffect(() => {
