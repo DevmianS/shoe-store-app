f24a1115d28cb15883ea486078f8789ea3ad76c6
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-26 21:38:00 +0200

    add tooltip

--- src/components/UI/Cart/Cart.js
+++ src/components/UI/Cart/Cart.js
@@ -1,9 +1,22 @@
 import Link from 'next/link';
 
+import {styled} from '@mui/material';
 import Badge from '@mui/material/Badge';
 import Box from '@mui/material/Box';
-
+import Tooltip, {tooltipClasses} from '@mui/material/Tooltip';
 import {theme} from '@/utils/theme';
+
+const HtmlTooltip = styled(({className, ...props}) => (
+  <Tooltip {...props} classes={{popper: className}} />
+))(({theme}) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: '#f5f5f9',
+    color: 'rgba(0, 0, 0, 0.87)',
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    border: '1px solid #dadde9',
+  },
+}));
 
 const cartStyles = {
   icon: {
@@ -25,10 +38,12 @@ const cartStyles = {
 };
 export default function Cart({count}) {
   return (
-    <Link href="/bag">
-      <Badge badgeContent={count} color="success" sx={cartStyles.badge}>
-        <Box sx={cartStyles.icon} className="icon-bag" variant="button" />
-      </Badge>
-    </Link>
+    <HtmlTooltip title="Go to the cart">
+      <Link href="/bag">
+        <Badge badgeContent={count} color="success" sx={cartStyles.badge}>
+          <Box sx={cartStyles.icon} className="icon-bag" variant="button" />
+        </Badge>
+      </Link>
+    </HtmlTooltip>
   );
 }
