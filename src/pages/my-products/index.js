73977bfc5e3e53b91d062dba55f93f4419b2d52d
v.lozghachov <vlozghachov.laba@solvd.com>
2023-05-27 22:36:33 +0200

    move styles my products outside page component

--- src/pages/my-products/index.js
+++ src/pages/my-products/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 import {Typography, Box, useMediaQuery, useTheme} from '@mui/material';
 
-import {rwdValue} from '@/utils/theme';
+import {rwdValue, theme} from '@/utils/theme';
 import bannerImg from '@/assets/banner.jpg';
 
 import SideBar from '@/components/Layout/SideBar';
@@ -18,70 +18,64 @@ import useMyProducts from '@/hooks/useMyProducts';
 import {SkeletonProducts} from '@/utils/utils';
 import NoContent from '@/components/UI/NoContent';
 
+const myProductsStyles = {
+  row: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingTop: rwdValue(0, 40),
+    paddingBottom: rwdValue(0, 40),
+  },
+  header: {
+    flex: '1 1 auto',
+    paddingLeft: rwdValue(10, 60),
+    paddingRight: rwdValue(10, 60),
+  },
+  avatarWrapper: {
+    marginLeft: rwdValue(20, 50),
+    marginBottom: rwdValue(20, 30),
+    flexDirection: 'row',
+    alignItems: 'end',
+    marginTop: {xs: '-15px', md: '-30px'},
+  },
+  avatar: {
+    marginRight: rwdValue(5, 15),
+    border: '4px solid white',
+    zIndex: 2,
+  },
+  name: {fontSize: rwdValue(14, 20)},
+  points: {
+    fontSize: rwdValue(12, 15),
+    color: theme.palette.text.tetriary,
+    marginBottom: {xs: 0, md: rwdValue(0, 12)},
+  },
+  h1: {marginBottom: rwdValue(20, 35)},
+  productsRow: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    margin: {xs: '0 -8px', md: '0 -24px'},
+  },
+  msgBody: {maxWidth: '320px', textAlign: 'center', margin: '0 auto'},
+  msgIcon: {
+    fontSize: 20,
+    width: 72,
+    height: 72,
+    borderRadius: '50%',
+    background: '#F9FAFB',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '50px auto 10px',
+  },
+  msgTitle: {fontSize: rwdValue(16, 20), marginBottom: '10px'},
+  msgText: {
+    fontSize: rwdValue(12, 15),
+    marginBottom: rwdValue(32, 40),
+  },
+  msgBtn: {maxWidth: '152px'},
+};
+
 const MyProducts = ({productsList}) => {
-  const theme = useTheme();
-  const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-
   const {products, isLoading} = useMyProducts();
-
-  console.log('products MYPRODUCTS: ', products, isLoading);
-
-  const styles = {
-    row: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      paddingTop: rwdValue(0, 40),
-      paddingBottom: rwdValue(0, 40),
-    },
-    header: {
-      flex: '1 1 auto',
-      paddingLeft: rwdValue(10, 60),
-      paddingRight: rwdValue(10, 60),
-    },
-    avatarWrapper: {
-      marginLeft: rwdValue(20, 50),
-      marginBottom: rwdValue(20, 30),
-      flexDirection: 'row',
-      alignItems: 'end',
-      marginTop: isMobile || isTablet ? '-15px' : '-30px',
-    },
-    avatar: {
-      marginRight: rwdValue(5, 15),
-      border: '4px solid white',
-      zIndex: 2,
-    },
-    name: {fontSize: rwdValue(14, 20)},
-    points: {
-      fontSize: rwdValue(12, 15),
-      color: theme.palette.text.tetriary,
-      marginBottom: isMobile ? 0 : rwdValue(0, 12),
-    },
-    h1: {marginBottom: rwdValue(20, 35)},
-    productsRow: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      margin: isMobile ? '0 -8px' : '0 -24px',
-    },
-    msgBody: {maxWidth: '320px', textAlign: 'center', margin: '0 auto'},
-    msgIcon: {
-      fontSize: 20,
-      width: 72,
-      height: 72,
-      borderRadius: '50%',
-      background: '#F9FAFB',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      margin: '50px auto 10px',
-    },
-    msgTitle: {fontSize: rwdValue(16, 20), marginBottom: '10px'},
-    msgText: {
-      fontSize: rwdValue(12, 15),
-      marginBottom: rwdValue(32, 40),
-    },
-    msgBtn: {maxWidth: '152px'},
-  };
 
   return (
     <>
@@ -89,9 +83,9 @@ const MyProducts = ({productsList}) => {
         <title>Wellrun | My Products</title>
       </Head>
       <NavBarLayout>
-        <Box sx={styles.row}>
+        <Box sx={myProductsStyles.row}>
           <SideBar />
-          <Box sx={styles.header}>
+          <Box sx={myProductsStyles.header}>
             <TopBanner imgPath={bannerImg.src} />
             <AvatarStaticLayout />
             <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
@@ -99,17 +93,10 @@ const MyProducts = ({productsList}) => {
                 My products
               </Typography>
               {Array.isArray(products) && products.length > 0 && (
-                <Link href="/add-product">
-                  <Button
-                    size={isMobile || isTablet ? 'small' : 'medium'}
-                    sx={styles.msgBtn}
-                  >
-                    Add product
-                  </Button>
-                </Link>
+                <Button sx={myProductsStyles.msgBtn}>Add product</Button>
               )}
             </Box>
-            <Box sx={styles.productsRow}>
+            <Box sx={myProductsStyles.productsRow}>
               {isLoading ? (
                 SkeletonProducts()
               ) : Array.isArray(products) && products.length > 0 ? (
